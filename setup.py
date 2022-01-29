47d9dd102844fbcd11313c81a5ef90d0bd8ec20d
Masaki Kobayashi <bayashi.cl@gmail.com>
2022-01-30 08:23:23 +0900

    poetryを使うのをやめる

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name="procon-grassmaker",
+    version="0.0.2",
+    author="Masaki Kobayash",
+    author_email="bayashi.cl@gmail.com",
+    install_requires=[
+        "requests",
+        "bs4",
+        "GitPython",
+        "python-dateutil",
+        "toml",
+        "colorama",
+        "colorlog",
+        "dacite",
+    ],
+    entry_points={
+        "console_scripts": ["procon-grassmaker = procon_grassmaker.main:main"]
+    },
+)
